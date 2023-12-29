15a3f03fab892e924082543faf814f9e930deabc
Pablo Goulart <pablogoulart02@gmail.com>
2023-12-29 20:05:05 -0300

    Implementação de sistema de vitória e empate

--- JS/game.js
+++ JS/game.js
@@ -16,6 +16,32 @@ function setupGame()
                     
     }
 
+    function checkWinner()
+    {
+        if (
+            (cells[0].textContent === currentPlayer && cells[1].textContent === currentPlayer && cells[2].textContent === currentPlayer) ||
+            (cells[3].textContent === currentPlayer && cells[4].textContent === currentPlayer && cells[5].textContent === currentPlayer) ||
+            (cells[6].textContent === currentPlayer && cells[7].textContent === currentPlayer && cells[8].textContent === currentPlayer) ||
+            (cells[0].textContent === currentPlayer && cells[3].textContent === currentPlayer && cells[6].textContent === currentPlayer) ||
+            (cells[1].textContent === currentPlayer && cells[4].textContent === currentPlayer && cells[7].textContent === currentPlayer) ||
+            (cells[2].textContent === currentPlayer && cells[5].textContent === currentPlayer && cells[8].textContent === currentPlayer) ||
+            (cells[0].textContent === currentPlayer && cells[4].textContent === currentPlayer && cells[8].textContent === currentPlayer) ||
+            (cells[2].textContent === currentPlayer && cells[4].textContent === currentPlayer && cells[6].textContent === currentPlayer)
+        ) {
+            document.querySelector('.finish-game').textContent = 'Player ' + currentPlayer + ' venceu!';
+            cells.forEach(cell => {
+                cell.textContent = '';
+            });
+        }
+        if (Array.from(cells).every(cell => cell.textContent !== ''))
+        {
+            document.querySelector('.finish-game').textContent = 'Empate!';
+            cells.forEach(cell => {
+                cell.textContent = '';
+            });
+        }
+    }
+
     cells.forEach(cell => {
         cell.addEventListener('click', function(event)
         {
@@ -23,6 +49,7 @@ function setupGame()
             {
                 cell.textContent = currentPlayer;
 
+                checkWinner();
                 switchPlayer();
             }
         });
